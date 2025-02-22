559cddb351779e12791a7a4d8150b643434462d7
Imad <imadchowdhury01@gmail.com>
2025-02-22 21:57:46 +0000

    Connect Navbar links to event listeners

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './styles.css'
+import { loadHome } from './home.js'
+import { loadMenu } from './menu.js'
+import { loadAbout } from './about.js'
 
-console.log('hello')
+const conent = document.querySelector('#content');
+const home = document.querySelector('#home');
+const menu = document.querySelector('#menu'); // TODO
+const about = document.querySelector('#about'); // TODO
+
+function clearContent() {
+    conent.textContent = '';
+}
+
+function handleNavClick(loadingFunction) {
+    clearContent();
+    loadingFunction()
+}
+
+home.addEventListener('click', () => {handleNavClick(loadHome)});
+menu.addEventListener('click',  () => {handleNavClick(loadMenu)});
+about.addEventListener('click',  () => {handleNavClick(loadAbout)});
